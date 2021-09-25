ccfc7a2427297b5ac75a5522b7b443819643a5af
Alejandra1249 <87240183+Alejandra1249@users.noreply.github.com>
2021-09-25 10:11:47 -0600

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	$email		= $_POST['email'];
 	$password	= md5($_POST['password']);
 
-	$sql = "SELECT email, password FROM member WHERE email = '$email' AND password = '$password'";
+	$sql = "SELECT email, password FROM usuarios WHERE email = '$email' AND password = '$password'";
 	$result = $conn->query($sql);
 
 	if ($result->num_rows == 1) {
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
 		$_SESSION['email'] = $email;
 
-		redirect("admin.php");
+		redirect("conferencistas.php");
 		exit;
 	} else {
 		set_message('<div class="alert alert-warning" role="alert" col-md-12"><p>Wrong username or password.</p></div>');
