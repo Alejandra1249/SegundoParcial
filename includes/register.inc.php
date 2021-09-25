a66ab695b92672cd8339a31607c4b6a2b636efcc
Alejandra1249 <87240183+Alejandra1249@users.noreply.github.com>
2021-09-25 10:12:18 -0600

    Update register.inc.php

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -1,9 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-	$first_name				= $_POST['first_name'];
-	$last_name				= $_POST['last_name'];
-	$display_name			= $_POST['display_name'];
 	$email					= $_POST['email'];
 	$password				= md5($_POST['password']);
 	$confirm_password		= $_POST['confirm_password'];
@@ -20,14 +17,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 			validation_errors($error);
 		}
 	}else{
-		$sql = "INSERT INTO member (first_name, last_name, display_name, email, password, date_added)
-		VALUES ('$first_name', '$last_name', '$display_name', '$email', '$password', now())";
+		$sql = "INSERT INTO segparcial.usuarios (email, password, date_added)
+		VALUES ('$email', '$password', now())";
 
 		if ($conn->query($sql) === TRUE) {
 			redirect("index.php");
 			exit;
 
-		} else { 
+		} else {
 			set_message("<p>Error: " . $sql . "<br>" . $conn->error . "</p>");
 		}
 
